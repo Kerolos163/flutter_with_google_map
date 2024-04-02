9909a7979b97a46ec52c180babfbd3f9e26facb7
kerolos <AhmedHassanTamait>
2024-04-03 01:22:34 +0200

    Add location service initialization and permission check
    Initialize the Location service and add a method to check and request
    permission for location services in the CustomGoogleMap widget. This ensures
    proper handling of location services in the widget.

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_with_google_map/models/place_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
@@ -15,6 +16,7 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
   late GoogleMapController mapController;
+  late Location location;
   Set<Marker> markers = {};
   Set<Polyline> polylines = {};
   Set<Polygon> polygons = {};
@@ -23,10 +25,12 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   void initState() {
     initialCameraPosition = const CameraPosition(
         zoom: 14.0, target: LatLng(29.305103116405615, 30.84337813070837));
-    initeMarkers();
-    initePolylines();
-    initePolygons();
-    initCircles();
+    // initeMarkers();
+    // initePolylines();
+    // initePolygons();
+    // initCircles();
+    location = Location();
+    checkandRequestPermission();
     super.initState();
   }
 
@@ -150,5 +154,15 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
         center: const LatLng(29.300117456644816, 30.833421321779685),
         radius: 1000);
     circles.add(circle);
+  }
+
+  void checkandRequestPermission() async {
+    var isServiceEnabled = await location.serviceEnabled();
+    if (!isServiceEnabled) {
+      isServiceEnabled = await location.requestService();
+      if (!isServiceEnabled) {
+        //to do
+      }
+    }
   }
 }
